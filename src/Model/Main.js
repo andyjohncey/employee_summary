2bdb371c789e91e420fc2740af1d2b0143dcb0bf
andrew.johncey@outlook.com <andrew.johncey@outlook.com>
2020-03-04 20:10:20 +1100

    call write file function

--- src/Model/Main.js
+++ src/Model/Main.js
@@ -1,13 +1,14 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
-const path = require("path");
-const util = require("util");
-const ejs = require("ejs")
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const ejs = require('ejs');
 
-const Engineer = require("./Engineer");
-const Intern = require("./Intern");
-const Manager = require("./Manager");
+const Engineer = require('./Engineer');
+const Intern = require('./Intern');
+const Manager = require('./Manager');
 const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 class Main {
     constructor() {
@@ -15,98 +16,29 @@ class Main {
     }
 
     async _hard() {
-        const templatesPath = (path.resolve(_dirname, "..", "templates", "main.ejs")
-        
-        const ejsTemplate = await readFileAsync(path.resolve(_dirname, "..", "templates", "main.ejs"), "utf-8 ");
+        const templatePath = path.resolve(__dirname, '..', 'templates', 'main.ejs');
 
-        const result = ejs.render(ejs.ejsTemplate, {
+        const ejsTemplate = await readFileAsync(templatePath, 'utf-8');
+
+        const result = ejs.render(ejsTemplate, {
             teamMembers: this._teamArray
         }, {
-            fileName: templatesPath
-        )
-    };
-    console.log(result);
+            filename: templatePath
+        });
+
+        console.log(result);
+
+        await writeFileAsync(path.resolve(__dirname, '..', 'dist', 'hard.html'), result);
+    }
+
 
 
     async run() {
-        const { teamSize } = await inquirer.prompt([{
-            type: "input",
-            name: "teamSize",
-            message: "Please input your team size",
-            default: 2
-        }]);
-
-
-        // for (let i = 0; i < teamSize; i++) {
-        //     console.log('---------------------------');
-        //     const response = await inquirer.prompt([
-        //         {
-        //             type: "input",
-        //             name: "name",
-        //             message: "Please input your name",
-        //         },
-        //         {
-        //             type: "input",
-        //             name: "email",
-        //             message: "Please input your email",
-        //         },
-        //         {
-        //             type: "list",
-        //             name: "role",
-        //             message: "Please input your role",
-        //             choices: [
-        //                 'Main.ENGINEER',
-        //                 'Main.INTERN',
-        //                 'Main.MANAGER'
-        //             ]
-        //         },
-        //         {
-        //             type: "input",
-        //             name: "github",
-        //             message: "Please input your github",
-        //             when: ({ role }) => role === "Main.ENGINEER",
-        //         },
-        //         {
-        //             type: "input",
-        //             name: "school",
-        //             message: "Please input your school",
-        //             when: ({ role }) => role === "Main.INTERN",
-        //         },
-        //         {
-        //             type: "input",
-        //             name: "officeNumber",
-        //             message: "Please input your officeNumber",
-        //             when: ({ role }) => role === "Main.MANAGER",
-        //         },
-        //     ]);
-
-        //     const {
-        //         name,
-        //         email,
-        //         role,
-        //         github,
-        //         school,
-        //         officeNumber,
-        //     } = response;
-
-        //     if (role === 'Main.ENGINEER') {
-        //         this._teamArray.push(new Engineer(name, email, github));
-        //     }
-        //     if (role === 'Main.INTERN') {
-        //         this._teamArray.push(new Intern(name, email, school));
-        //     }
-        //     if (role === 'Main.MANAGER') {
-        //         this._teamArray.push(new Manager(name, email, officeNumber));
-        //     }
-        // }
-
-
-
 
         this._teamArray = [
-            new Engineer("engineer name", "engineer email", engineer github);
-            new Intern("intern name", "intern email", intern school);
-            new Manager("manager name", "manager email", manager office number);
+            new Engineer("engineer name", "engineer email", "engineer github"),
+            new Intern("intern name", "intern email", "intern school"),
+            new Manager("manager name", "manager email", "manager office number"),
         ]
         await this._hard();
     }
